501230b9bc48bbbf5fbc8de8a2cf3741f78f7965
laisdejesus <dejesusll@outlook.com>
2021-07-21 23:22:41 -0300

    SignUpPage ok

--- labmusic/src/pages/SignUp/SignUpPage.js
+++ labmusic/src/pages/SignUp/SignUpPage.js
@@ -1,12 +1,71 @@
 import React from 'react'
+import useForm from '../../hooks/useForm'
+import { useHistory } from 'react-router-dom'
+import {goToSignUp} from "../../routes/coordinator"
+import { signup } from '../../services/user'
 
 
 const SignUpPage = () => {
-  
+  const [form, onChange, clear] = useForm({ email: "", password: "" })
+  const history = useHistory()
+
+
+  const onSubmitForm = (event) => {
+    event.preventDefault()
+    signup(form, clear, history)
+  }
 
   return (
     <div>
-        Sou o SignUpPage
+        <form onSubmit={onSubmitForm}>
+            <input
+                name={"name"}
+                value={form.name}
+                onChange={onChange}
+                label={"E-mail"}
+                fullWidth
+                required
+                type={"text"}
+            />
+            <input
+                name={"nickname"}
+                value={form.nickname}
+                onChange={onChange}
+                label={"E-mail"}
+                fullWidth
+                required
+                type={"text"}
+            />
+            <input
+                name={"email"}
+                value={form.email}
+                onChange={onChange}
+                label={"E-mail"}
+                fullWidth
+                required
+                type={"email"}
+            />
+            <input
+                name={"password"}
+                value={form.password}
+                onChange={onChange}
+                label={"Senha"}
+                fullWidth
+                required
+                type={"password"}
+            />
+            <button
+              type={"submit"}
+              fullWidth
+            
+            >
+              Cadastrar
+
+            </button>
+        </form>
+
+        <a onClick={() => goToSignUp(history)}> Fazer Login</a>
+
     </div>
   )
 }
--- labmusic/src/services/user.js
+++ labmusic/src/services/user.js
@@ -17,18 +17,14 @@ export const login = (body, clear, history) => {
         })
 }
 
-// export const signUp = (body, clear, history, setRightButtonText, setIsLoading) => {
-//     setIsLoading(true)
-//     axios.post(`${BASE_URL}/user/signup`, body)
-//         .then((res) => {
-//             localStorage.setItem("token", res.data.token)
-//             clear()
-//             setIsLoading(false)
-//             goToRecipesList(history)
-//             setRightButtonText("Logout")
-//         })
-//         .catch((err) => {
-//             setIsLoading(false)
-//             alert(err.response.data.message)
-//         })
-// }
+export const signup = (body, clear, history) => {
+    axios.post(`${BASE_URL}/user/signup`, body)
+        .then((res) => {
+            localStorage.setItem("token", res.data.token)
+            clear()
+            goToFeed(history)
+        })
+        .catch((err) => {
+            alert(err.response.data.message)
+        })
+}
